adbea86db614573ef99403c1b2603c66db045aca
Cihad Saglam <saglam.chd@gmail.com>
2019-08-07 17:14:57 +0200

    created diffirent 2 table and worked on it with MVC in real app but need to work on Frontend part

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,13 +1,54 @@
 class RestaurantsController < ApplicationController
-  def edit
-  end
+  # before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
 
+  # GET /restaurants
   def index
+    @restaurants = Restaurant.all
   end
 
-  def new
-  end
-
+  # GET /restaurants/1
   def show
+    @restaurant = Restaurant.find(params[:id])
   end
+
+  # GET /restaurants/new
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  # GET /restaurants/1/edit
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  # POST /restaurants
+  def create
+    @restaurant = Restaurant.new(restaurant_strong_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  def update
+    @restaurant = @restaurant = Restaurant.find(params[:id])
+    @restaurant.update(restaurant_strong_params)
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  def destroy
+    @restaurant.destroy
+    redirect_to @restaurant_path
+  end
+
+  private
+
+  def restaurant_strong_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+
+  # def set_restaurant
+  #   @restaurant = Restaurant.find(params[:id])
+  # end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,47 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+Review.destroy_all
+
+puts 'Creating restaurants...'
+
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  '01 43 54 23 31',
+    category:      'chinese'
+  },
+  {
+    name:         'Mahama',
+    address:      '4 BArce St, London E7 5Qp',
+    phone_number:  '01 34 54 67 12',
+    category:      'indian'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number:  '01 43 24 23 86',
+    category:       'italian'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+
+reviews_attributes = [
+  {
+    content:  'Buzzy destination for Indian street food in Bombay-style vintage decor.',
+    rating:   5,
+    restaurant_id: Restaurant.last.id
+  },
+  {
+    content:  'Pizzeria with industrial looks, serving rustic pizza and antipasti.',
+    rating:   4,
+    restaurant_id: Restaurant.first.id
+  }
+]
+
+Review.create!(reviews_attributes)
+
+puts 'Finished!'
